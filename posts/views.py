a5f952c51f9322635778b9dddb50d5fc31109fca
Alfonzo Juliana <alfonzoj@ev-o3.kzn.21-school.ru>
2022-05-20 19:43:14 +0300

    cmt3

---
+++ posts/views.py
@@ -0,0 +1,9 @@
+from re import template
+from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
+
+
+class HomePageView(ListView):
+    model = Post
+    template_name = "home.html"
